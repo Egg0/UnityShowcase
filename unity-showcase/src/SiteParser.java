3e45d7db8d01ce92925fde71ab39f894b51e0380
Egg0 <marcuslr@live.com>
2017-11-11 02:00:50 -0800

    Initial test: jsoup scraping

--- unity-showcase/src/SiteParser.java
+++ unity-showcase/src/SiteParser.java
@@ -1,4 +1,19 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class SiteParser {
 
+	public static void main(String[] args) throws IOException {
+		Document doc = Jsoup.connect("https://unity3d.com/showcase/gallery").get();
+		//System.out.println(doc.data());
+		Elements games = doc.getAllElements();
+		for (Element game : games) {
+			System.out.println(game.data());
+		}
+		
+	}
 }
